74d308eed4172dfeeacb8f629147369039d913d3
Andrés Giraldo <afgiraldoestrada@gmail.com>
2024-11-28 19:21:40 -0500

    Actualización

--- Domain/Interfaces/IProductoService.cs
+++ Domain/Interfaces/IProductoService.cs
@@ -4,11 +4,11 @@ namespace ShoppingAPI_Jueves_2023II.Domain.Interfaces
 {
     public interface IProductoService
     {
-        Task<IEnumerable<Producto>> GetProductoAsync();
-        Task<Producto> CreateProductoAsync(Producto producto);
-        Task<Producto> GetProductoByIdAsync(Guid id);
-        Task<Producto> GetProductoByNameAsync(string name);
-        Task<Producto> EditProductoAsync(Producto producto);
-        Task<Producto> DeleteProductoAsync(Guid id);
+        Task<IEnumerable<Producto>>GetProductoAsync();
+        Task<Producto>CreateProductoAsync(Producto producto);
+        Task<Producto>GetProductoByIdAsync(Guid id);
+        Task<Producto>GetProductoByNameAsync(string name);
+        Task<Producto>EditProductoAsync(Producto producto);
+        Task<Producto>DeleteProductoAsync(Guid id);
     }
 }
